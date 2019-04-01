745a75a4e11981035b74b742d401f658200878ba
nacho <colba@MacBook-Pro-de-nacho.local>
2019-04-01 09:03:22 +0200

    creating objects

---
+++ javascript-thp/object.js
@@ -0,0 +1,40 @@
+/* Put functionality and data bundled in one object */
+// object literal
+const user1 = {
+  name: "Nacho",
+  score: 4,
+  increment: function() {
+    user1.score++;
+  }
+};
+user1.increment();
+
+// creating user2 user dot notation
+const user2 = {}; // create a const in memory, same position in memory with an empty object
+user2.name = "Lucía";
+user2.score = 5;
+user2.increment = function() {
+  user2.score++;
+};
+
+// creating user3 user oBJECT.CREATE
+const user3 = Object.create(null);
+user3.name = "Eva";
+user3.score = 9;
+user3.increment = function() {
+  user3.score++;
+};
+
+// creating the object with a function without handcrafting each object
+function userCreator(name, score) {
+  const newUser = {};
+  newUser.name = name;
+  newUser.score = score;
+  newUser.increment = function() {
+    newUser.score++;
+  };
+  return newUser;
+}
+const user1 = userCreator("Nacho", 4);
+const user2 = userCreator("Lucia", 5);
+user1.increment();

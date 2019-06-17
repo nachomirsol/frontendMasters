dad31163172d3767e8f733e967e192698d036e03
nacho <colba@MacBook-Pro-de-nacho.local>
2019-06-17 08:44:03 +0200

    solution 3

--- javascript-thp/subClassing_3.js
+++ javascript-thp/subClassing_3.js
@@ -28,3 +28,17 @@ paidUserCreator.prototype.increaseBalance = function() {
 const paidUser1 = new paidUserCreator("Lucia", 9, 30);
 paidUser1.increaseBalance();
 paidUser1.sayName();
+
+/**
+ *
+ * The first part we have the only difference with the new keyword, which does a little stuff automatically
+ * for us.
+ * In the execution context this object is created.
+ * 1- Creates the this label with the object in local execution context
+ * 2- sets the __proto__ hidden property boned to prototype functions
+ * 3- Return the object to global memory
+ *
+ * Then we declare a function paidUserCreator in global memory as a function object combo that has
+ * its own protototype with its proto (instead of setPrototypeOf, object.create) we override the userCreator object
+ * We want also this prototype object to link to the userCreator
+ */
